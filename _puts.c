1c0fe4051998f2ad05a127013e3a63325a9543cf
Hatim Bouzerioh <hatim.bouzerioh@gmail.com>
2023-07-23 16:08:44 -0700

    Write a function to print a string

---
+++ _puts.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - Return the length of a string
+ * @s: The string whose length will be returned
+ *
+ * Return: The length of the string s.
+ */
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+		length++;
+
+	return (length);
+}
+
+/**
+ * _puts - writes the string s to the stdout
+ * @s: The string to be printed
+ *
+ * Return: The length of the string or -1 if if fails.
+ */
+int _puts(const char *s)
+{
+	return (write(1, s, _strlen(s)));
+}
