ced4ab2eabbab13c34703e670c1a3690b94d3457
Hatim Bouzerioh <hatim.bouzerioh@gmail.com>
2023-07-24 09:04:29 -0700

    Update the file by adding the definition of the function _print_int

--- print_functions.c
+++ print_functions.c
@@ -18,7 +18,7 @@ __attribute__((unused)) const char *s, __attribute__((unused)) int n)
 
 
 /**
- * _print_string - Handls the conversion specifer s
+ * _print_string - Handle the conversion specifer s
  * @c: A character
  * @s: A pointer to a string
  * @n: An integer
@@ -30,4 +30,22 @@ __attribute__((unused)) const char *s, __attribute__((unused)) int n)
 {
 	return (_puts(s));
 }
+/**
+ * _print_int - handle the conversion specifiers related to numbers
+ * @c: A character
+ * @s: A pointer to a string
+ * @n: An integer
+ *
+ * Return: The number of bytes printed or - 1 if it fails.
+ */
+int _print_int(__attribute__((unused)) char c,
+__attribute__((unused)) const char *s, __attribute__((unused)) int n)
+{
+	int k;
+	if (c == 'b')
+		k = print_binary(n);
+	return (k);
+}
+
+
 
