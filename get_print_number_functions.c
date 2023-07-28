706cbbbe8f19e695e3fc1443db6fb10b4bca3264
Hatim Bouzerioh <hatim.bouzerioh@gmail.com>
2023-07-28 08:17:25 -0700

    Create a new function to get the appropritate print_number function

---
+++ get_print_number_functions.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * get_h - A pointer to a function
+ * @s: A pointer to a string
+ *
+ * Return: A pointer to the correct function. or NULL if there is no match
+ */
+void (*get_h(const char *s))(unsigned long int)
+{
+	int i = 0;
+
+	conv_spec_h conv_specs[] = {
+		{ "b", print_binary},
+		{ "d", print_integer},
+		{ "i", print_integer},
+		{ "u", print_integer},
+		{ "o", print_octal},
+		{ "x", print_hexadecimal_x},
+		{ "X", print_hexadecimal_X},
+		{NULL, NULL}
+	};
+
+	while (conv_specs[i].conv_spec_ != NULL)
+	{
+		if (*s == *(conv_specs[i].conv_spec_))
+		{
+			return (conv_specs[i].h);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
