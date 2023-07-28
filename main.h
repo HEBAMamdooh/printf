e13ccb291dedcab1ae01dcdfe9c5cfa6f797ad7d
Hatim Bouzerioh <hatim.bouzerioh@gmail.com>
2023-07-28 07:42:36 -0700

    Update the header file

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 /**
- * struct conv_spec - Strct conv_spec
+ * struct conv_spec - Struct conv_spec
  * @conv_spec: The conversion specifier
  * @f: The function associated
  */
@@ -13,6 +13,17 @@ typedef struct conv_spec
 	char *conv_spec;
 	int (*f)(char c, const char *s, long int n, unsigned long int m);
 } conv_spec_t;
+
+/**
+ * struct conv_spec_ - Struct conv spec
+ * @conv_spec_: The conversion specifier
+ * @h: The function associated
+ */
+typedef struct conv_spec_
+{
+	char *conv_spec;
+	void (*h)(unsigned long int n);
+} conv_spec_h;
 
 int _putchar(char c);
 int _puts(const char *s);
@@ -23,6 +34,7 @@ int _print_char(char c, const char *s, long int n, unsigned long int m);
 int _print_string(char c, const char *s, long int n, unsigned long int m);
 int _print_int(char c, const char *s, long int n, unsigned long int m);
 int (*get_f(const char *s))(char, const char *, long int, unsigned long int);
+void (*get_h(const char *s))(unsigned long int n);
 
 void print_binary(unsigned long int n);
 void print_integer(unsigned long int n);
